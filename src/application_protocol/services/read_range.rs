8a8137de2efafffe8f4b693a32fb9a0b7cf99923
David Haig <david@ninjametal.com>
2023-10-20 17:59:00 +0100

    Fixed serde issue

--- src/application_protocol/services/read_range.rs
+++ src/application_protocol/services/read_range.rs
@@ -148,7 +148,7 @@ pub struct ReadRangeItems<'a> {
 
 #[derive(Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum ReadRangeValue {
     Status,
     Bool(bool),
